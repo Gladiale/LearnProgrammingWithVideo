2a5af391e59789b61a5d99887158469e4dad208b
Gladiale <the_Garden_of_sinners@protonmail.com>
2024-07-01 21:37:07 +0800

    💥 色々更新しました

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import flet as ft
-import pyautogui
 import keyboard
 import json
+import time
 
-json_open = open("config.json", "r")
-json_load = json.load(json_open)
+# 設定ファイルを開く
+with open("config.json", "r") as json_open:
+    json_load = json.load(json_open)
+hot_key = json_load['hot_key']
 
-videoData = json_load["video_coordinates"]
-keyData = json_load['hot_key']
+# 目標サイト
+target_site = {
+    "youtube": "k",
+    "udemy": "enter"
+}
+play_key = ""
 
+# print(pyautogui.KEYBOARD_KEYS)
 def click_target():
-    # 現在の座標を取得
-    mouseX, mouseY = pyautogui.position()
-    pyautogui.click(videoData["x"], videoData["y"])
-    pyautogui.click(mouseX, mouseY)
+    if (play_key):
+        # keyboardライブラリを使用してキーをシミュレーション
+        keyboard.press('alt')
+        keyboard.press_and_release('tab')
+        keyboard.release('alt')
+        time.sleep(0.5)
+        keyboard.press_and_release(play_key)
+        keyboard.press('alt')
+        keyboard.press_and_release('tab')
+        keyboard.release('alt')
+    else:
+        pass
 
 def main(page: ft.Page):
-    page.title = "LeaningProgram"
+    page.title = "HelperCodingLearning"
     page.window.width = 320
-    page.window.height = 240
+    page.window.height = 180
     page.padding = 10
     page.spacing = 0
     page.window.resizable = False  # ウインドウサイズ変更可否
@@ -28,30 +43,37 @@ def main(page: ft.Page):
     page.window.maximizable= False
     # page.window.title_bar_hidden=True
 
-    # ---------画面サイズ取得---------
-    x, y = pyautogui.size()
-    screen_size = ft.Text(f"画面サイズ: x {x}px, y {y}px")
+    def radiogroup_changed(e):
+        global play_key
+        play_key = e.control.value
     
-    video_coordinates = ft.Text(f"動画設定座標: x {videoData['x']}px, y {videoData['y']}px")
-    hot_key = ft.Text(f"ホットキー: {keyData}")
+    radio = ft.RadioGroup(
+        ft.Row(
+            [
+            ft.Radio(value=target_site["youtube"] ,label="Youtube", splash_radius=10, fill_color=ft.colors.PURPLE_ACCENT_400),
+            ft.Radio(value=target_site["udemy"], label="Udemy", splash_radius=10, fill_color=ft.colors.PURPLE_ACCENT_400),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        on_change=radiogroup_changed
+    )
 
     content = ft.Column(
         [   
-            screen_size, 
+            ft.Text(f"ホットキー: {hot_key}"),
             ft.Divider(height=1, color=ft.colors.PURPLE_400),
-            video_coordinates,
-            ft.Divider(height=1, color=ft.colors.PURPLE_400),
-            hot_key
+            radio
         ],
-        # expand=1,
+        expand=1,
         spacing=10,
         alignment=ft.MainAxisAlignment.CENTER,
-        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
     )
 
     page.add(content)
 
     # suppress=True を設置することで、パソコンデフォルトのHotkeyの抑制可能
-    keyboard.add_hotkey(keyData, click_target, args=[], suppress=True)
+    # trigger_on_releaseこのオプションをTrueに設定すると、ホットキーの動作はキーが離されたときに実行されます(Hotkey抑制できない問題を完全に解決できる)
+    keyboard.add_hotkey(hot_key, click_target, args=[], suppress=True, trigger_on_release=True)
 
 ft.app(target=main)
