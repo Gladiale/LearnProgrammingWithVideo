ddec893fb5fc81539232177a3ab9f23a7615826a
Gladiale <the_Garden_of_sinners@protonmail.com>
2024-07-01 15:36:49 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import flet as ft
+import pyautogui
+import keyboard
+import json
+
+json_open = open("config.json", "r")
+json_load = json.load(json_open)
+
+videoData = json_load["video_coordinates"]
+editorData = json_load["editor_coordinates"]
+keyData = json_load['hot_key']
+
+def click_target():
+    pyautogui.click(videoData["x"], videoData["y"])
+    pyautogui.click(editorData["x"], editorData["y"])
+
+def main(page: ft.Page):
+    page.title = "LeaningProgram"
+    page.window.width = 320
+    page.window.height = 240
+    page.padding = 10
+    page.spacing = 0
+    page.window.resizable = False  # ウインドウサイズ変更可否
+    page.window.center()  # ウィンドウをデスクトップの中心に移動
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER  # 水平方向の中央寄せ
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER # 縦方向中央寄せ
+    page.window.maximizable= False
+    # page.window.title_bar_hidden=True
+
+    # ---------画面サイズ取得---------
+    x, y = pyautogui.size()
+    screen_size = ft.Text(f"画面サイズ: x {x}px, y {y}px")
+    
+    video_coordinates = ft.Text(f"動画設定座標: x {videoData['x']}px, y {videoData['y']}px")
+    edit_coordinates = ft.Text(f"エディタ設定座標: x {editorData['x']}px, y {editorData['y']}px")
+    hot_key = ft.Text(f"ホットキー: {keyData}")
+
+    content = ft.Column(
+        [   
+            screen_size, 
+            ft.Divider(height=1, color=ft.colors.PURPLE_400),
+            video_coordinates,
+            ft.Divider(height=1, color=ft.colors.PURPLE_400),
+            edit_coordinates,
+            ft.Divider(height=1, color=ft.colors.PURPLE_400),
+            hot_key
+        ],
+        # expand=1,
+        spacing=10,
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    )
+
+    page.add(content)
+
+    # suppress=True を設置することで、パソコンデフォルトのHotkeyの抑制可能
+    keyboard.add_hotkey(keyData, click_target, args=[], suppress=True)
+
+ft.app(target=main)
