3e0d3cc6bf343cd6ffd62f4054cec2d4a620f125
Gladiale <the_Garden_of_sinners@protonmail.com>
2024-07-02 21:18:03 +0800

    ✨ 色々更新

--- main.py
+++ main.py
@@ -7,28 +7,26 @@ import time
 with open("config.json", "r") as json_open:
     json_load = json.load(json_open)
 hot_key = json_load['hot_key']
+delay = json_load["delay"]
 
 # 目標サイト
 target_site = {
     "youtube": "k",
     "udemy": "enter"
 }
-play_key = ""
+play_key = target_site["youtube"]
 
 # print(pyautogui.KEYBOARD_KEYS)
 def click_target():
-    if (play_key):
-        # keyboardライブラリを使用してキーをシミュレーション
-        keyboard.press('alt')
-        keyboard.press_and_release('tab')
-        keyboard.release('alt')
-        time.sleep(0.5)
-        keyboard.press_and_release(play_key)
-        keyboard.press('alt')
-        keyboard.press_and_release('tab')
-        keyboard.release('alt')
-    else:
-        pass
+    # keyboardライブラリを使用してキーをシミュレーション
+    keyboard.press('alt')
+    keyboard.press_and_release('tab')
+    keyboard.release('alt')
+    time.sleep(delay)
+    keyboard.press_and_release(play_key)
+    keyboard.press('alt')
+    keyboard.press_and_release('tab')
+    keyboard.release('alt')
 
 def main(page: ft.Page):
     page.title = "HelperCodingLearning"
@@ -43,26 +41,50 @@ def main(page: ft.Page):
     page.window.maximizable= False
     # page.window.title_bar_hidden=True
 
-    def radiogroup_changed(e):
+    YoutubeRef = ft.Ref[ft.TextButton]()
+    UdemyRef = ft.Ref[ft.TextButton]()
+
+    def setTarget(e):
         global play_key
-        play_key = e.control.value
-    
-    radio = ft.RadioGroup(
-        ft.Row(
-            [
-            ft.Radio(value=target_site["youtube"] ,label="Youtube", splash_radius=10, fill_color=ft.colors.PURPLE_ACCENT_400),
-            ft.Radio(value=target_site["udemy"], label="Udemy", splash_radius=10, fill_color=ft.colors.PURPLE_ACCENT_400),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        on_change=radiogroup_changed
+        target = e.control.text
+        match (target):
+            case "Youtube":
+                play_key = target_site["youtube"]
+            case "Udemy":
+                play_key = target_site["udemy"]
+            case _:
+                pass
+        YoutubeRef.current.icon = (
+            "CHECK" if(play_key==target_site["youtube"]) else "CHECK_BOX_OUTLINE_BLANK_SHARP"
+        )
+        UdemyRef.current.icon = (
+            "CHECK" if(play_key==target_site["udemy"]) else "CHECK_BOX_OUTLINE_BLANK_SHARP"
+        )
+        control_button.update()
+
+    control_button = ft.Row(
+        [
+            ft.TextButton("Youtube", icon="CHECK" if(play_key==target_site["youtube"]) else "CHECK_BOX_OUTLINE_BLANK_SHARP", on_click=setTarget, ref=YoutubeRef),
+            ft.TextButton("Udemy", icon="CHECK" if(play_key==target_site["udemy"]) else "CHECK_BOX_OUTLINE_BLANK_SHARP", on_click=setTarget, ref=UdemyRef),
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
     )
 
     content = ft.Column(
         [   
-            ft.Text(f"ホットキー: {hot_key}"),
-            ft.Divider(height=1, color=ft.colors.PURPLE_400),
-            radio
+            ft.Text(f"ホットキー: {hot_key}",
+                    style=ft.TextStyle(
+                                size=20,
+                                weight=ft.FontWeight.NORMAL,
+                                foreground=ft.Paint(
+                                    color=ft.colors.PURPLE_ACCENT_700,
+                                    stroke_width=1,
+                                    stroke_join=ft.StrokeJoin.ROUND,
+                                    style=ft.PaintingStyle.STROKE,
+                                ))
+                    ),
+            ft.Divider(height=1, color=ft.colors.PURPLE_600),
+            control_button
         ],
         expand=1,
         spacing=10,
